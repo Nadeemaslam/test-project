c4274f7ad86f756aea954a1f5c163124585538d0
nadeem <er.nadeemaslam89@gmail.com>
2015-06-15 13:05:31 +0530

    first commit

---
+++ blog/views.py
@@ -0,0 +1,12 @@
+
+from django.shortcuts import render
+
+from .models import Question
+
+
+
+
+def index(request):
+    latest_question_list = Question.objects.order_by('-pub_date')[:5]
+    context = {'latest_question_list': latest_question_list}
+    return render(request, 'blog/index.html', context)
